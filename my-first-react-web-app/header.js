b1a95b202dd3e0fc018ddec454a3b5713df0f51f
ismet <88513656+ismetbakay10@users.noreply.github.com>
2022-02-19 00:36:42 +0100

    Add files via upload

---
+++ my-first-react-web-app/header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="Header">
+      <Nav defaultActiveKey="/home" as="ul">
+        <Nav.Item as="li">
+          <Nav.Link href="/home">Active</Nav.Link>
+        </Nav.Item>
+        <Nav.Item as="li">
+          <Nav.Link eventKey="link-1">Link</Nav.Link>
+        </Nav.Item>
+        <Nav.Item as="li">
+          <Nav.Link eventKey="link-2">Link</Nav.Link>
+        </Nav.Item>
+      </Nav>
+    </div>
+  );
+}
+
+export default Header;
